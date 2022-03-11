ac9f0ab06680773cb412fcc2c901b7cc1692d35d
Kevin Gann <kevin.gann@rev.com>
2022-03-11 09:45:43 -0500

    Adding some breadscrumbs.

--- first_code.tf
+++ first_code.tf
@@ -7,6 +7,9 @@ resource "aws_s3_bucket" "tf_course" {
     bucket = "tf-course-kg-2022-03-11"
 }
 
+/* 
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-4-upgrade#acl-argument
+*/
 resource "aws_s3_bucket_acl" "tf_course" {
     bucket = aws_s3_bucket.tf_course.id
       acl = "private"
